8e2930b7bbf6770dd2e208b4e524599ab8b158b8
peter <piggy_77772002@yahoo.com>
2026-02-07 00:56:57 +0800

    1

---
+++ practice2.js
@@ -0,0 +1,30 @@
+const fs=require("fs");
+const qs=require("querystring");
+const dotenv=require('dotenv');
+const { Client } = require('pg');
+
+dotenv.config(); 
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }  // Supabase 一定要
+});
+
+async function main() {
+  let result;
+  await client.connect();
+
+  result = await client.query(`
+      INSERT INTO student (student_id,name) VALUES ($1,$2);
+    `,[1,'peter']
+  );
+  //console.log(result.rows);
+
+
+  await client.end();
+}
+
+main().catch(console.error)
+
+
+
